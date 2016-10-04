7744feb4d800657dce32c1277a6e8925b044b30e
Alex Barstow <alexander.barstow@gmail.com>
2016-10-04 12:56:18 -0400

    2 quick hound edits

--- spec/support/factory_girl.rb
+++ spec/support/factory_girl.rb
@@ -4,7 +4,7 @@ FactoryGirl.define do
   factory :cheese do
     sequence(:name) { |n| "Vermont Sharp White Cheddar #{n}" }
     sequence(:description) { |n| "this is my #{n}th favorite cheese" }
-    sequence(:age) {|n| n }
-    sequence(:user_id) {|n| n}
+    sequence(:age) { |n| n }
+    sequence(:user_id) { |n| n }
   end
 end
