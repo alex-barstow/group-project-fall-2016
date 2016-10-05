f8ff4149565915fd36ac67f8fb97fb868e565f84
Alex Barstow <alexander.barstow@gmail.com>
2016-10-05 17:06:49 -0400

    WIP ajax votes

--- app/assets/javascripts/votes.js
+++ app/assets/javascripts/votes.js
@@ -4,14 +4,15 @@ $(() => {
     upvoteButtons.click(event => {
       event.preventDefault();
       let reviewId = event.target.attributes["data-review"].value;
+
       $.ajax({
-        method: 'POST'
-        url: `/reviews/${reviewId}/vote`
-        data: 1
-      })
-      .done(data => {
-        debugger;
-      })
+        method: 'POST',
+        url: `/reviews/${reviewId}/votes`,
+        data: {vote: 1}
+      });
+      // .done(data => {
+      //   debugger;
+      // })
     })
   }
 })
