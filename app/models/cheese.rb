2d16ef08f0a179e9747478ec724118de68e33d28
Stephen MacVicar <iwantmymoneyback@gmail.com>
2016-10-05 12:31:38 -0400

    Hound fixes

--- app/models/cheese.rb
+++ app/models/cheese.rb
@@ -3,5 +3,6 @@ class Cheese < ActiveRecord::Base
 
   validates :name, presence: true
   validates :user_id, presence: true
-  validates :age, allow_blank: true, format: { with: /\d+ (months?|days?|years?)\z/}
+  validates :age, allow_blank: true,
+                  format: { with: /\d+ (months?|days?|years?)\z/ }
 end
