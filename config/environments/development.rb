c160e7bcc6b2920ae884bf87f02f1442dadc81fd
Alex Barstow <alexander.barstow@gmail.com>
2016-10-07 14:09:33 -0400

    hound edits round 1

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -55,9 +55,9 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
 
   config.action_mailer.smtp_settings = {
-    address: "localhost",
+    address: 'localhost',
     port: 1025
-  } 
+  }
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
