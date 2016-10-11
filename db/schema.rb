2994347c58a3f9d32a18a4e01e19d2f62d2b4b54
Alex Barstow <alexander.barstow@gmail.com>
2016-10-11 16:01:05 -0400

    additional styling, foundation being annoying

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161004194928) do
+ActiveRecord::Schema.define(version: 20161005181429) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,10 +23,11 @@ ActiveRecord::Schema.define(version: 20161004194928) do
   end
 
   create_table "reviews", force: :cascade do |t|
-    t.integer "rating",    null: false
+    t.integer "rating",                 null: false
     t.text    "body"
-    t.integer "user_id",   null: false
-    t.integer "cheese_id", null: false
+    t.integer "user_id",                null: false
+    t.integer "cheese_id",              null: false
+    t.integer "vote_total", default: 0
   end
 
   create_table "users", force: :cascade do |t|
@@ -45,9 +46,16 @@ ActiveRecord::Schema.define(version: 20161004194928) do
     t.string   "user_name"
     t.string   "first_name"
     t.string   "last_name"
+    t.string   "avatar"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
     t.index ["user_name"], name: "index_users_on_user_name", unique: true, using: :btree
+  end
+
+  create_table "votes", force: :cascade do |t|
+    t.integer "vote",      null: false
+    t.integer "user_id",   null: false
+    t.integer "review_id", null: false
   end
 
 end
