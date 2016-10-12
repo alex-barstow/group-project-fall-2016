a0ac0b554dc36003309cbc2471cbb9536955f12d
Stephen MacVicar <iwantmymoneyback@gmail.com>
2016-10-12 14:36:20 -0400

    users can delete their own reviews. tests green

--- spec/features/admin_role_spec.rb
+++ spec/features/admin_role_spec.rb
@@ -11,6 +11,7 @@ feature 'Admin users have special privileges' do
     fill_in 'user_password', with: admin.password
     click_button 'Sign In'
     visit users_path
+    
     expect(page).to have_current_path(users_path)
     expect(page).to have_content(user.user_name)
     expect(page).to have_content(admin.user_name)
@@ -22,6 +23,7 @@ feature 'Admin users have special privileges' do
     fill_in 'user_password', with: user.password
     click_button 'Sign In'
     visit users_path
+
     expect(page).to have_current_path(root_path)
     expect(page).to have_content('Insufficient access rights')
   end
@@ -33,6 +35,7 @@ feature 'Admin users have special privileges' do
     click_button 'Sign In'
     visit users_path
     click_link(user.user_name)
+
     expect(page).to have_current_path(user_path(user))
   end
 
@@ -44,7 +47,24 @@ feature 'Admin users have special privileges' do
     visit cheese_path(review.cheese)
     review_body = review.body
     click_link 'X'
+
     expect(page).to have_content('Review deleted')
     expect(page).to_not have_content(review_body)
   end
+
+  scenario 'user can only see delete button for their own reviews' do
+    visit new_user_session_path
+    fill_in 'Email', with: review.user.email
+    fill_in 'user_password', with: review.user.password
+    click_button 'Sign In'
+    visit cheese_path(review.cheese)
+
+    expect(page).to have_link('X')
+
+    new_review = FactoryGirl.create(:review)
+    visit cheese_path(new_review.cheese)
+
+    expect(page).to_not have_link('X')
+  end
+
 end
