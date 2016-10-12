9277b663394fc4b48a2e9c44fd32634bcc54b156
Stephen MacVicar <iwantmymoneyback@gmail.com>
2016-10-12 16:52:39 -0400

    removed sleep for codeship

--- spec/features/01_sees_cheese_list_spec.rb
+++ spec/features/01_sees_cheese_list_spec.rb
@@ -5,8 +5,6 @@ feature 'user sees a list of reviewed cheeses' do
 
   scenario 'the cheeses name is visible', js: true do
     visit root_path
-    sleep 10
-    p page.driver.console_messages
     expect(page).to have_content(cheese.name)
   end
 
