0f70370d8e3d994e474473b6974c4b830a9c0793
Alex Barstow <alexander.barstow@gmail.com>
2016-10-13 15:21:10 -0400

    search finished, tests green, styles meh

--- spec/features/cheese_search_spec.rb
+++ spec/features/cheese_search_spec.rb
@@ -7,9 +7,18 @@ feature 'Anyone can search the cheese list on the index page' do
   scenario 'entering string into search bar narrows list to cheeses that contain that string', js: true do
     visit root_path
     fill_in 'Search', with: 'Vermont'
-    click_button 'Search'
+    click_link 'Search'
 
     expect(page).to have_content(cheese1.name)
+    expect(page).to_not have_content(cheese2.name)
+  end
+
+  scenario 'after search, ajax request does not reload full cheese list' do
+    visit root_path
+    fill_in 'Search', with: 'Vermont'
+    click_link 'Search'
+
+    sleep(6)
     expect(page).to_not have_content(cheese2.name)
   end
 
