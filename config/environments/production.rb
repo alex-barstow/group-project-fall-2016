834f09dab10601f0334159af5dd4693dc5e1a792
Alex Barstow <alexander.barstow@gmail.com>
2016-10-13 17:58:05 -0400

    more sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,14 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  ActionMailer::Base.smtp_settings = {
+  user_name: 'app57788359@heroku.com',
+  password: ENV['SENDGRID_PASSWORD'],
+  domain: 'https://boston-neighborhoods-reviews.herokuapp.com/',
+  address: 'smtp.sendgrid.net',
+  port: 587,
+  authentication: :plain,
+  enable_starttls_auto: true
+  }
 end
