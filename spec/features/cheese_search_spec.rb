aede75066ceeb620c22e1179550ba4689a4edab9
Alex Barstow <alexander.barstow@gmail.com>
2016-10-13 14:35:11 -0400

    added search functionality, need a few more tests

---
+++ spec/features/cheese_search_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+feature 'Anyone can search the cheese list on the index page' do
+  let!(:cheese1) {FactoryGirl.create(:cheese, name: 'Vermont Sharp')}
+  let!(:cheese2) {FactoryGirl.create(:cheese, name: 'Pepper Jack')}
+
+  scenario 'entering string into search bar narrows list to cheeses that contain that string', js: true do
+    visit root_path
+    fill_in 'Search', with: 'Vermont'
+    click_button 'Search'
+
+    expect(page).to have_content(cheese1.name)
+    expect(page).to_not have_content(cheese2.name)
+  end
+
+end
