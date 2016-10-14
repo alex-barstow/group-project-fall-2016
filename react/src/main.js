001ab8ea9fdc7d3cf31567697ce479c4132c385e
Stephen MacVicar <iwantmymoneyback@gmail.com>
2016-10-14 11:02:43 -0400

    changed app to App

--- react/src/main.js
+++ react/src/main.js
@@ -1,7 +1,7 @@
 import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './app';
+import App from './App';
 
 $(function() {
   console.log("I'm react!")
